e0438c9cc240d472f09a34e033a3d04216a78db2
Mantry <vg1317178267@gmail.com>
2026-02-17 10:20:34 -0500

    Project created

---
+++ src/main/java/sheridan/runze/midtermexam/MidtermExamApplication.java
@@ -0,0 +1,13 @@
+package sheridan.runze.midtermexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidtermExamApplication.class, args);
+    }
+
+}
